cc122e8382d1ce517d6d6ef6b3ec535fbc57db0e
raphael <raph@streetsmarts.io>
2019-11-25 22:58:07 -0700

    14-done

--- 10-Sourcefiles/myapp.js
+++ 10-Sourcefiles/myapp.js
@@ -1,13 +1,70 @@
-console.log( "Hello world" );
+/*console.log( "hellow world" );
 
-console.log( 200 );
+console.log( "200" );
 
 console.log( true );
 
 console.log( null );
 
 var num = 10;
-
 console.log( num );
 
-console.log( "string", 10.11, true, false, null, num );
+console.log( "string", 10.11, true, false, null, num);
+
+console.log("this is a string");
+*/
+
+/*function makeCoffee( sugar, milk )
+{
+    
+    var instructions = " Boil water ";
+    
+    instructions += " pour into cup,";
+    
+    instructions += "add coffee granules,";
+    
+    instructions += " add " + sugar + " spoons of sugar,";
+    
+    instructions += " add " + milk + "% milk.";
+    
+    return instructions;
+}
+
+console.log ( makeCoffee( 2, 20 ) );
+*/
+
+/* var car = {
+    color : "red",
+    speed : 200,
+    drive : function(){ return "drive"; }
+};
+
+var shoppinglist = [
+    "Apple",
+    "Orange",
+    "Pear"
+];
+*/
+
+var car = {
+    make: "volvo",
+    speed: 160,
+    engine: {
+        size: 2.0,
+        make: "BMW",
+        fuel: "petrol",
+        pistons: [ {maker: "BMW" }, { maker:"BMW" } ]
+    },
+    drive: function(){return "drive"; }
+};
+
+var array = [
+    "string", 
+    100, 
+    [ "embed", 200 ],
+    { car: "ford" },
+    function(){ return "drive"; }
+];
+
+
+
